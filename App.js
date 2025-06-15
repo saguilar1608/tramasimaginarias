fe331939a9e753abd61bc56a0dca97aeab7d805d
saguilar1608 <sofiaa_1608@outlook.it>
2025-06-16 00:25:19 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
+      <h1 style={{ fontSize: '8vw', textAlign: 'center' }}>Tramas Imaginarias</h1>
+    </div>
+  );
+}
+
+export default App;
